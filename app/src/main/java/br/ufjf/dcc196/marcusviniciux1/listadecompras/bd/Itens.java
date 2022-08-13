8c917b0dc0fa12fc79e91ed05bfc60feb87aa4d8
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2022-08-13 17:54:51 -0300

    implementa a Entity Itens

--- app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/Itens.java
+++ app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/Itens.java
@@ -1,4 +1,18 @@
 package br.ufjf.dcc196.marcusviniciux1.listadecompras.bd;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Itens {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+    @ColumnInfo(name = "nomeItem")
+    public String itemName;
+    @ColumnInfo(name = "idCategoria")
+    public int idCategoria;
+    @ColumnInfo(name = "completo")
+    public boolean completo;
+
 }
