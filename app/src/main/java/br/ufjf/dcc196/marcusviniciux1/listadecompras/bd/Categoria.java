373e9ab6f1f18476b4d8dbb32e729b12c8d908d9
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2022-08-13 17:49:11 -0300

    implementa a Entity Categoria

--- app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/Categoria.java
+++ app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/Categoria.java
@@ -1,4 +1,14 @@
 package br.ufjf.dcc196.marcusviniciux1.listadecompras.bd;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Categoria {
+    @PrimaryKey(autoGenerate = true)
+    public int uid;
+    @ColumnInfo(name = "nomeCategoria")
+    public String nomeCategoria;
+
 }
