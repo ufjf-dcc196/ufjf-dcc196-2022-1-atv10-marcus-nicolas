f8ba15875c4e976838d5501c70ffdaa502df542a
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2022-08-13 18:10:15 -0300

    adiciona operacao CRUD basica

--- app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/ListaDeComprasDAO.java
+++ app/src/main/java/br/ufjf/dcc196/marcusviniciux1/listadecompras/bd/ListaDeComprasDAO.java
@@ -1,7 +1,36 @@
 package br.ufjf.dcc196.marcusviniciux1.listadecompras.bd;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface ListaDeComprasDAO {
+    @Query("Select * from Categoria")
+    List<Categoria> getTodasListasDeCategorias();
+
+    @Insert
+    void inserirCategoria(Categoria...categorias);
+
+    @Update
+    void atualizarCategoria(Categoria...categorias);
+
+    @Delete
+    void deletarCategoria(Categoria...categorias);
+
+    @Query("Select * from Itens where idCategoria = :idCat")
+    List<Itens> getTodasListasDeItens(int idCat);
+
+    @Insert
+    void inserirItens(Itens itens);
+
+    @Update
+    void atualizarItens(Itens itens);
+
+    @Delete
+    void deleteItens(Itens itens);
 }
